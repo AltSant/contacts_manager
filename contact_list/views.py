13690261af19a954b094ef8545196144bd14693e
AltSant <akirjyanov@yandex.ru>
2022-08-14 17:38:18 +0300

    add view and main template

--- contact_list/views.py
+++ contact_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Contact
+
+def contact_list(request):
+    contact_ob = Contact.objects.all()
+    return render(request, 'contact_list/contact_list.html', {'contact_ob':contact_ob})
 
 # Create your views here.
