9e61107a0971b4947a969bdf3376aa706ef2bde7
AltSant <akirjyanov@yandex.ru>
2022-08-16 13:21:37 +0300

    add search to main page

--- contact_list/views.py
+++ contact_list/views.py
@@ -1,17 +1,25 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.db.models import Q
 from .models import Contact
 from .forms import ContactForm
 
 def contact_list(request):
-    contact_ob = Contact.objects.all()
+    
+    search_contact = request.GET.get('search')
+   
+    if search_contact:
+         contact_ob= Contact.objects.filter(Q(full_name__icontains=search_contact))
+    else:
+        contact_ob = Contact.objects.all()
     pagination = Paginator(contact_ob, 8)
     if 'page' in request.GET:
         page_num = request.GET['page']
     else:
         page_num =1
-    page = pagination.get_page(page_num)        
+    page = pagination.get_page(page_num)
+    
     return render(request, 'contact_list/contact_list.html', {'contact_ob':page.object_list, 'page':page, 'pagination':pagination})
 
 def contact_detail(request, pk):
@@ -50,7 +58,13 @@ def contact_delete(request, pk):
         contact.delete()
         return redirect('contact_list')
     except Contact.DoesNotExist:
-        return HttpResponseNotFound("<h2>Person not found</h2>")       
+        return HttpResponseNotFound("<h2>Person not found</h2>")    
+
+def contact_search(request):
+    search_contact = request.GET.get('search')
+    contacts_find = Contact.objects.filter(Q(full_name__icontains=search_contact))
+    return render(request, 'contact_list/contact_list.html', {'contact_ob':contacts_find} )
+
     
 
 # Create your views here.
